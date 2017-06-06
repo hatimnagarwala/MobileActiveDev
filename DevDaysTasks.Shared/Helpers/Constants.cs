f4ccdae6b9ba22e960ab2ac723408adc068532ee
hatim nagarwala <a-hanaga@microsoft.com>
2017-06-06 05:24:00 +0400

    Add project files.

---
+++ DevDaysTasks.Shared/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevDaysTasks
+{
+	public static class Constants
+	{
+        // Your mobile service endpoint URL. Replace strings with your mobile services and gateway URLs.
+        public static string ApplicationURL = @"https://{Your-App-Link}.azurewebsites.net";
+        // The name of your local database
+        public static readonly string SyncStorePath = "syncstore.db";
+    }
+}
+
